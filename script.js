59ec7860cd07aaafe8f423ed640d63a4f5c9a554
Px228 <115811073+Px228@users.noreply.github.com>
2023-08-27 22:51:33 +0300

    v1.6

--- script.js
+++ script.js
@@ -93,18 +93,47 @@ function startGame() {
   // Обновляем положение камеры
   updateCameraPosition();
 }
-
 let isMovingUp = false;
 let isMovingDown = false;
 let isMovingLeft = false;
 let isMovingRight = false;
-// ...
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      isMovingUp = true;
+      break;
+    case "ArrowDown":
+      isMovingDown = true;
+      break;
+    case "ArrowLeft":
+      isMovingLeft = true;
+      break;
+    case "ArrowRight":
+      isMovingRight = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      isMovingUp = false;
+      break;
+    case "ArrowDown":
+      isMovingDown = false;
+      break;
+    case "ArrowLeft":
+      isMovingLeft = false;
+      break;
+    case "ArrowRight":
+      isMovingRight = false;
+      break;
+  }
+});
 
 function moveCharacter() {
-  // Обработка движения с клавиатуры
-  handleKeyboardInput();
-
-  // Обработка движения с джойстика
+  // Получение базового значения шага в зависимости от уровня скорости
   const baseStep = 10;
   const adjustedStep = baseStep * speedLevel; // Используйте актуальный уровень скорости
 
@@ -128,31 +157,8 @@ function moveCharacter() {
   updateCharacterPosition();
   updateCameraPosition();
   checkCollision();
-}
+} 
 
-// ...
-
-function handleKeyboardInput() {
-  const baseStep = 10;
-  const adjustedStep = baseStep * speedLevel;
-
-  if (isMovingUp) {
-    characterY -= adjustedStep;
-  }
-  if (isMovingDown) {
-    characterY += adjustedStep;
-  }
-  if (isMovingLeft) {
-    characterX -= adjustedStep;
-  }
-  if (isMovingRight) {
-    characterX += adjustedStep;
-  }
-
-  // Проверяем границы карты
-  characterX = Math.max(0, Math.min(containerWidth - characterWidth, characterX));
-  characterY = Math.max(0, Math.min(containerHeight - characterHeight, characterY));
-}
 
 // ...
 
@@ -225,8 +231,6 @@ function updateCameraPosition() {
 
   updateMiniMap();
 }
-
-
 
 function spawnBalls() {
   const numberOfBalls = 500;
@@ -318,10 +322,188 @@ function closePauseModal() {
 }
 
 function openSettings() {
-  // Добавьте код для открытия окна настроек
-  // Например, изменение настроек игры, управление и т. д.
-  console.log("Открыть окно настроек");
+  // Display the settings modal
+  settingsModal.style.display = "block";
+
+  // Load and set the current control setting
+  const controlType = localStorage.getItem("controlType");
+  if (controlType === "joystick") {
+    document.querySelector("input[value='joystick']").checked = true;
+  } else if (controlType === "keyboard") {
+    document.querySelector("input[value='keyboard']").checked = true;
+  }
+
+  // Add event listener to control setting radios
+  const controlRadios = document.querySelectorAll("input[name='controlType']");
+  controlRadios.forEach(radio => {
+    radio.addEventListener("change", handleControlChange);
+  });
 }
+function handleControlChange(event) {
+  const selectedControlType = event.target.value;
+
+  // Save the selected control type to localStorage
+  localStorage.setItem("controlType", selectedControlType);
+
+  // Update the control method based on selection
+  if (selectedControlType === "joystick") {
+    enableJoystickControl();
+  } else if (selectedControlType === "keyboard") {
+    enableKeyboardControl();
+  }
+
+  // Close the settings modal
+  settingsModal.style.display = "none";
+}
+function enableJoystickControl() {
+  const joystick = document.querySelector(".joystick");
+  const stick = document.querySelector(".stick");
+
+  let isMoving = false; // Flag to track joystick movement
+  let stickX = 0;
+  let stickY = 0;
+
+  stick.addEventListener("mousedown", handleTouchStart);
+  document.addEventListener("mousemove", handleTouchMove);
+  document.addEventListener("mouseup", handleTouchEnd);
+
+  // Event handler for touch start
+  function handleTouchStart(event) {
+    event.preventDefault();
+    isMoving = true;
+  }
+
+  // Event handler for touch move
+  function handleTouchMove(event) {
+    if (!isMoving) return;
+    event.preventDefault();
+
+    const joystickRect = joystick.getBoundingClientRect();
+    stickX = event.clientX - joystickRect.left - stick.offsetWidth / 2;
+    stickY = event.clientY - joystickRect.top - stick.offsetHeight / 2;
+    updateStickPosition();
+  }
+
+  // Event handler for touch end
+  function handleTouchEnd(event) {
+    isMoving = false;
+    stickX = 0;
+    stickY = 0;
+    updateStickPosition();
+  }
+
+  // Function to update stick position and move the character
+  function updateStickPosition() {
+    const maxDistance = joystick.offsetWidth / 2 - stick.offsetWidth / 2;
+    const distance = Math.min(Math.sqrt(stickX ** 2 + stickY ** 2), maxDistance);
+    const angle = Math.atan2(stickY, stickX);
+
+    const offsetX = distance * Math.cos(angle);
+    const offsetY = distance * Math.sin(angle);
+
+    stick.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+
+    moveCharacter(offsetX, offsetY); // Call the character movement function
+  }
+
+  // ...
+
+  // Function for moving the character based on joystick input
+  function moveCharacter(offsetX, offsetY) {
+    const step = 10;
+
+    if (offsetX > 0) {
+      characterX += step;
+    } else if (offsetX < 0) {
+      characterX -= step;
+    }
+
+    if (offsetY > 0) {
+      characterY += step;
+    } else if (offsetY < 0) {
+      characterY -= step;
+    }
+
+    // Check map boundaries
+    characterX = Math.max(0, Math.min(containerWidth - characterWidth, characterX));
+    characterY = Math.max(0, Math.min(containerHeight - characterHeight, characterY));
+
+    updateCharacterPosition();
+    updateCameraPosition();
+    checkCollision();
+  }
+}
+
+function enableKeyboardControl() {
+  // Adjust your game's logic for enabling keyboard control
+  // For example, listen to keyboard events and handle character movements.
+  let isMovingUp = false;
+  let isMovingDown = false;
+  let isMovingLeft = false;
+  let isMovingRight = false;
+
+  window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        isMovingUp = true;
+        break;
+      case "ArrowDown":
+        isMovingDown = true;
+        break;
+      case "ArrowLeft":
+        isMovingLeft = true;
+        break;
+      case "ArrowRight":
+        isMovingRight = true;
+        break;
+    }
+  });
+
+  window.addEventListener("keyup", (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        isMovingUp = false;
+        break;
+      case "ArrowDown":
+        isMovingDown = false;
+        break;
+      case "ArrowLeft":
+        isMovingLeft = false;
+        break;
+      case "ArrowRight":
+        isMovingRight = false;
+        break;
+    }
+  });
+
+  function moveCharacter() {
+    // Получение базового значения шага в зависимости от уровня скорости
+    const baseStep = 10;
+    const adjustedStep = baseStep * speedLevel; // Используйте актуальный уровень скорости
+
+    if (isMovingUp) {
+      characterY -= adjustedStep;
+    }
+    if (isMovingDown) {
+      characterY += adjustedStep;
+    }
+    if (isMovingLeft) {
+      characterX -= adjustedStep;
+    }
+    if (isMovingRight) {
+      characterX += adjustedStep;
+    }
+
+    // Проверяем границы карты
+    characterX = Math.max(0, Math.min(containerWidth - characterWidth, characterX));
+    characterY = Math.max(0, Math.min(containerHeight - characterHeight, characterY));
+
+    updateCharacterPosition();
+    updateCameraPosition();
+    checkCollision();
+  } 
+}
+
 
 
 function closeShopModal() {
@@ -354,82 +536,7 @@ function updateSpeedLevelDisplay() {
 }
 
 
-const joystick = document.querySelector(".joystick");
-const stick = document.querySelector(".stick");
 
-let isMoving = false; // Flag to track joystick movement
-let stickX = 0;
-let stickY = 0;
-
-stick.addEventListener("mousedown", handleTouchStart);
-document.addEventListener("mousemove", handleTouchMove);
-document.addEventListener("mouseup", handleTouchEnd);
-
-// Event handler for touch start
-function handleTouchStart(event) {
-  event.preventDefault();
-  isMoving = true;
-}
-
-// Event handler for touch move
-function handleTouchMove(event) {
-  if (!isMoving) return;
-  event.preventDefault();
-
-  const joystickRect = joystick.getBoundingClientRect();
-  stickX = event.clientX - joystickRect.left - stick.offsetWidth / 2;
-  stickY = event.clientY - joystickRect.top - stick.offsetHeight / 2;
-  updateStickPosition();
-}
-
-// Event handler for touch end
-function handleTouchEnd(event) {
-  isMoving = false;
-  stickX = 0;
-  stickY = 0;
-  updateStickPosition();
-}
-
-// Function to update stick position and move the character
-function updateStickPosition() {
-  const maxDistance = joystick.offsetWidth / 2 - stick.offsetWidth / 2;
-  const distance = Math.min(Math.sqrt(stickX ** 2 + stickY ** 2), maxDistance);
-  const angle = Math.atan2(stickY, stickX);
-
-  const offsetX = distance * Math.cos(angle);
-  const offsetY = distance * Math.sin(angle);
-
-  stick.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-
-  moveCharacter(offsetX, offsetY); // Call the character movement function
-}
-
-// ...
-
-// Function for moving the character based on joystick input
-function moveCharacter(offsetX, offsetY) {
-  const step = 10;
-
-  if (offsetX > 0) {
-    characterX += step;
-  } else if (offsetX < 0) {
-    characterX -= step;
-  }
-
-  if (offsetY > 0) {
-    characterY += step;
-  } else if (offsetY < 0) {
-    characterY -= step;
-  }
-
-  // Check map boundaries
-  characterX = Math.max(0, Math.min(containerWidth - characterWidth, characterX));
-  characterY = Math.max(0, Math.min(containerHeight - characterHeight, characterY));
-
-  updateCharacterPosition();
-  updateCameraPosition();
-  checkCollision();
-}
 
 
 
